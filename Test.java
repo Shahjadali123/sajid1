cd77f533dc4c0c827af86f324e9fecaa34a4de93
Shahjadali123 <90888130+Shahjadali123@users.noreply.github.com>
2023-01-14 13:58:52 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,20 @@
+package com.shahjad.ali;
+import org.springframework.beans.factory.BeanFactory;  
+import org.springframework.beans.factory.xml.XmlBeanFactory;  
+import org.springframework.core.io.ClassPathResource;  
+import org.springframework.core.io.Resource; 
+public class Test {
+
+	public static void main(String[] args) {
+		
+		
+		 Resource resource=new ClassPathResource("abc.xml");  
+		    BeanFactory factory=new XmlBeanFactory(resource);  
+		      
+		    Student student=(Student)factory.getBean("studentbean");  
+		    student.displayInfo();  
+		// TODO Auto-generated method stub
+
+	}
+
+}
